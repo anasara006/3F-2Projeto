48aefbb93c313baacd4e2433fd70183a05838e42
anasara006 <110102304+anasara006@users.noreply.github.com>
2024-11-08 08:48:11 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,40 +1,40 @@
 import { getCSS } from "./comum.js";
-async function quantidaDeUsuarios(){
-   const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-   const res = await fetch(url);
-   const dados = await res.json();
-   const nomeDasRedes = Object.keys(dados);
-   const quantidaDeUsuarios = Object.values(dados);
+async function quantidaDeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url);
+    const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidaDeUsuarios = Object.values(dados);
 
 
-   const infos = [
-    {
-        x: nomeDasRedes,
-        y: quantidaDeUsuarios,
-        type: 'bar',
-        marker:{
-            color: getCSS('--cor-primaria')
+    const infos = [
+        {
+            x: nomeDasRedes,
+            y: quantidaDeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-primaria')
+            }
+        }
+    ]
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title: {
+            text: 'redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--cor-primaria'),
+                family: getCSS('--font'),
+                size: 30
+            }
         }
     }
-   ]
-   const layout = {
-    plot_bgcolor: getCSS('--cor-de-fundo'),
-    paper_bgcolor: getCSS('--cor-de-fundo'),
-    title: {
-        text: 'redes sociais com mais usuários no mundo',
-        x: 0,
-        font:{
-            color:getCSS('--cor-primaria'),
-            family: getCSS('--font'),
-            size: 30
-        }
-    }
-   }
-   const grafico = document.createElement('div');
-   grafico.className = 'grafico';
-   document.getElementById('graficos- contairner').appendChild(grafico);
-   Plotly.newPlot(grafico,infos,layout);
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos- contairner').appendChild(grafico);
+    Plotly.newPlot(grafico, infos, layout);
 
 }
 
-quantidaDeUsuarios ();
+quantidaDeUsuarios();
